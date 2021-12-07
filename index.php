8679a0b9a734874561a2d3eceb31e79cd972fc0d
NathLib <94648053+NathLib@users.noreply.github.com>
2021-12-07 18:09:35 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <html>
+  <head>
+    <title>Divinity</title>
+    </head>
   
-  
-  
-  
+  <body>
+    
+  <h1>Wellcome to Divinity</h1>
+  <p>Web is under development</p>
+    
+    </body>
   
   </html>
